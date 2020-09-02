50a64bebfef320b449b84234d0cd6de3ad03fed7
Marcelo Rodrigo de Souza <msouza7681@gmail.com>
2020-09-02 07:50:06 -0300

    Minhas customizações

--- script.js
+++ script.js
@@ -26,14 +26,27 @@ let shuffleOrder = () => {
 
 //acende a proxima cor
 let lightColor = (element, number) => {
-    number = number * 500;
+    number = number * 700;
     setTimeout(() => {
+        playAudio(element.classList[0], "play");
         element.classList.add('selected');
-    }, number - 250);
+    }, number - 500);
     setTimeout(() => {
+        playAudio(element.classList[0], "stop");
         element.classList.remove('selected');
-    });
+    }, number);
 }
+
+//play audio
+let playAudio = (className, act = "play") => {
+    let audio = document.getElementById(`${className}-sound`);
+    if (act === "play") {
+        audio.play();
+    } else {
+        audio.pause();
+    }
+}   
+
 
 //checa se os botoes clicados são os mesmos da ordem gerada no jogo
 let checkOrder = () => {
@@ -43,19 +56,27 @@ let checkOrder = () => {
             break;
         }
     }
+    
+    //alterar placar
     if(clickedOrder.length == order.length) {
-        alert(`Pontuação: ${score}\nVocê acertou! Iniciando próximo nível!`);
-        nextLevel();
+        //alert(`Pontuação: ${score}\nVocê acertou! Iniciando próximo nível!`);
+        messagePopup("Segue o jogo!", `Pontuação: ${score}\nVocê acertou! Iniciando próximo nível!`, 'Prosseguir', () => {
+            resetPopup()
+            nextLevel();
+        })
     }
 }
 
 //funcao para o clique do usuario
 let click = (color) => {
+    let elementColor = createColorElement(color);
     clickedOrder[clickedOrder.length] = color;
-    createColorElement(color).classList.add('selected');
+    elementColor.classList.add('selected');
+    playAudio(elementColor.classList[0], "play")
 
     setTimeout(() => {
         createColorElement(color).classList.remove('selected');
+        playAudio(elementColor.classList[0], "stop")
         checkOrder();
     },250);
 }
@@ -75,25 +96,63 @@ let createColorElement = (color) => {
 
 //funcao para proximo nivel do jogo
 let nextLevel = () => {
+    changeScoreView();
     score++;
     shuffleOrder();
 }
 
-//funcao para game over
-let gameOver = () => {
-    alert(`Pontuação: ${score}!\nVocê perdeu o jogo!\nClique em OK para iniciar um novo jogo`);
-    order = [];
-    clickedOrder = [];
-
-    playGame();
+//altera o score na view
+let changeScoreView = () => {
+    let html = `<h2>Acertos: ${score}</h2>`;
+    document.querySelector('.score').innerHTML = html;
 }
 
-//funcao de inicio do jogo
-let playGame = () => {
-    alert('Bem vindo ao Gênesis! Iniciando novo jogo!');
-    score = 0;
+//funcao para game over
+let gameOver = () => {
+    //alert(`Pontuação: ${score}!\nVocê perdeu o jogo!\nClique em OK para iniciar um novo jogo`);
+    messagePopup("Você Perdeu!",`Pontuação: ${score}!\nVocê perdeu o jogo!\nClique em OK para iniciar um novo jogo`,'Fazer O quê!', () => {
+        resetPopup();
+        order = [];
+        clickedOrder = [];
 
-    nextLevel();
+        playGame("Recomeçar");
+    })
+    
+}
+
+
+
+//função messages popup
+let messagePopup = (title, msg, screen, callback) => {
+
+    document.querySelector('.popup').style.display = "flex";
+
+    let elMsgTitle = document.querySelector('.message-box .message-title h1');
+    let elMsgContent = document.querySelector('.message-box .message-content');
+    let elMsgFooter = document.querySelector('.message-box .message-footer');
+    let btn_start = `<button class="btn">${screen}</button>`;
+
+    elMsgTitle.innerText = title;
+    elMsgContent.innerHTML = `<p>${msg}</p>`;
+    elMsgFooter.innerHTML = btn_start;
+
+    document.querySelector('.btn').addEventListener('click', callback);
+
+}
+
+let resetPopup = () => {
+    document.querySelector('.popup').style.display = "none";
+    document.querySelector('.btn').remove();
+}
+
+
+//funcao de inicio do jogo
+let playGame = (status) => {  
+    messagePopup('Bem Vindo!','Bem vindo ao Gênesis! Iniciando novo jogo!', status, () => {
+        resetPopup();
+        score = 0;
+        nextLevel();
+    });
 }
 
 //eventos de clique para as cores
@@ -104,4 +163,4 @@ blue.onclick = () => click(3);
 
 
 //inicio do jogo
-playGame();
+playGame("Start");
